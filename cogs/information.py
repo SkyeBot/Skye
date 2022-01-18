3a13dd1890d52bde2ededf9bffb1acea086dca36
SawshaDev <94498480+SawshaDev@users.noreply.github.com>
2022-01-17 16:03:51 -0800

    Create information.py

---
+++ cogs/information.py
@@ -0,0 +1,70 @@
+import discord, datetime, time
+from discord.ext import commands
+
+from utils import http, cache, default
+
+import psutil
+import aiohttp
+
+
+start_time = time.time()
+
+
+class Information(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def uptime(self, ctx):
+        current_time = time.time()
+        difference = int(round(current_time - start_time))
+        text = str(datetime.timedelta(seconds=difference))
+        embed = discord.Embed(colour=ctx.message.author.top_role.colour)
+        embed.add_field(name="Uptime", value=text)
+        try:
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
+            await ctx.send("Current uptime: " + text)
+
+    @commands.command()
+    async def ping(self, ctx):
+        before = time.monotonic()
+        before_ws = int(round(self.bot.latency * 1000, 1))
+        message = await ctx.send("🏓 Pong")
+        ping = (time.monotonic() - before) * 1000
+        await message.edit(content=f"Skye's current ping is WS: {before_ws}ms  |  REST: {int(ping)}ms")
+
+    @commands.command()
+    async def covid(self, ctx, *, country: str):
+        async with ctx.channel.typing():
+            r = await http.get(f"https://disease.sh/v3/covid-19/countries/{country.lower()}", res_method="json")
+
+            if "message" in r:
+                return await ctx.send(f"The API returned an error:\n{r['message']}")
+
+            json_data = [
+                ("Total Cases", r["cases"]), ("Total Deaths", r["deaths"]),
+                ("Total Recover", r["recovered"]), ("Total Active Cases", r["active"]),
+                ("Total Critical Condition", r["critical"]), ("New Cases Today", r["todayCases"]),
+                ("New Deaths Today", r["todayDeaths"]), ("New Recovery Today", r["todayRecovered"])
+            ]
+
+            embed = discord.Embed(
+                description=f"The information provided was last updated <t:{int(r['updated'] / 1000)}:R> \n sad covid :("
+            )
+
+            for name, value in json_data:
+                embed.add_field(
+                    name=name, value=f"{value:,}" if isinstance(value, int) else value
+                )
+
+            await ctx.send(
+                f"**COVID-19** statistics in :flag_{r['countryInfo']['iso2'].lower()}: "
+                f"**{country.capitalize()}** *({r['countryInfo']['iso3']})*",
+                embed=embed
+            )
+
+
+
+def setup(bot):
+    bot.add_cog(Information(bot))    
