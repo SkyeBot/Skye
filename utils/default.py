c9e95b41f104565593255fc02ab65fa99453fc55
SawshaDev <94498480+SawshaDev@users.noreply.github.com>
2022-01-17 16:01:24 -0800

    Add files via upload

---
+++ utils/default.py
@@ -0,0 +1,97 @@
+import time
+import json
+import discord
+import traceback
+import timeago as timesince
+import datetime
+import calendar
+
+from io import BytesIO
+
+
+def config(filename: str = "config"):
+    """ Fetch default config file """
+    try:
+        with open(f"{filename}.json", encoding='utf8') as data:
+            return json.load(data)
+    except FileNotFoundError:
+        raise FileNotFoundError("JSON file wasn't found")
+
+
+def traceback_maker(err, advance: bool = True):
+    """ A way to debug your code anywhere """
+    _traceback = ''.join(traceback.format_tb(err.__traceback__))
+    error = ('```py\n{1}{0}: {2}\n```').format(type(err).__name__, _traceback, err)
+    return error if advance else f"{type(err).__name__}: {err}"
+
+
+def timetext(name):
+    """ Timestamp, but in text form """
+    return f"{name}_{int(time.time())}.txt"
+
+
+def date(target, clock: bool = True, seconds: bool = False, ago: bool = False, only_ago: bool = False, raw: bool = False):
+    if isinstance(target, int) or isinstance(target, float):
+        target = datetime.datetime.utcfromtimestamp(target)
+
+    if raw:
+        if clock:
+            timestamp = target.strftime("%d %B %Y, %H:%M")
+        elif seconds:
+            timestamp = target.strftime("%d %B %Y, %H:%M:%S")
+        else:
+            timestamp = target.strftime("%d %B %Y")
+
+        if isinstance(target, int) or isinstance(target, float):
+            target = datetime.datetime.utcfromtimestamp(target)
+            target = calendar.timegm(target.timetuple())
+
+        if ago:
+            timestamp += f" ({timesince.format(target)})"
+        if only_ago:
+            timestamp = timesince.format(target)
+
+        return f"{timestamp} (UTC)"
+    else:
+        unix = int(time.mktime(target.timetuple()))
+        timestamp = f"<t:{unix}:{'f' if clock else 'D'}>"
+        if ago:
+            timestamp += f" (<t:{unix}:R>)"
+        if only_ago:
+            timestamp = f"<t:{unix}:R>"
+        return timestamp
+
+
+def responsible(target, reason):
+    """ Default responsible maker targeted to find user in AuditLogs """
+    responsible = f"[ {target} ]"
+    if not reason:
+        return f"{responsible} no reason given..."
+    return f"{responsible} {reason}"
+
+
+def actionmessage(case, mass=False):
+    """ Default way to present action confirmation in chat """
+    output = f"**{case}** the user"
+
+    if mass:
+        output = f"**{case}** the IDs/Users"
+
+    return f"✅ Successfully {output}"
+
+
+async def prettyResults(ctx, filename: str = "Results", resultmsg: str = "Here's the results:", loop=None):
+    """ A prettier way to show loop results """
+    if not loop:
+        return await ctx.send("The result was empty...")
+
+    pretty = "\r\n".join([f"[{str(num).zfill(2)}] {data}" for num, data in enumerate(loop, start=1)])
+
+    if len(loop) < 15:
+        return await ctx.send(f"{resultmsg}```ini\n{pretty}```")
+
+    data = BytesIO(pretty.encode('utf-8'))
+    await ctx.send(
+        content=resultmsg,
+        file=discord.File(data, filename=timetext(filename.title()))
+    )
---
+++ utils/http.py
@@ -0,0 +1,30 @@
+import aiohttp
+
+from utils import cache
+
+
+# Removes the aiohttp ClientSession instance warning.
+class HTTPSession(aiohttp.ClientSession):
+    """ Abstract class for aiohttp. """
+
+    def __del__(self):
+
+        if not self.closed:
+            self.close()
+
+
+session = HTTPSession()
+
+
+@cache.async_cache()
+async def query(url, method="get", res_method="text", *args, **kwargs):
+    async with getattr(session, method.lower())(url, *args, **kwargs) as res:
+        return await getattr(res, res_method)()
+
+
+async def get(url, *args, **kwargs):
+    return await query(url, "get", *args, **kwargs)
+
+
+async def post(url, *args, **kwargs):
+    return await query(url, "post", *args, **kwargs)
