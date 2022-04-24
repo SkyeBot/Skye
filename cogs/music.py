9ae19376692fd24f5a3139089356002e27917471
SawshaDev <contactcallimarie@gmail.com>
2022-04-24 14:13:16 +0000

    hi

--- cogs/music.py
+++ cogs/music.py
@@ -1,200 +1,71 @@
-import youtube_dl
+
+"""
+This example cog demonstrates basic usage of Lavalink.py, using the DefaultPlayer.
+As this example primarily showcases usage in conjunction with discord.py, you will need to make
+modifications as necessary for use with another Discord library.
+Usage of this cog requires Python 3.6 or higher due to the use of f-strings.
+Compatibility with Python 3.5 should be possible if f-strings are removed.
+"""
+import re
+
 import discord
-
-import aiohttp
-import asyncio
-
+import wavelink
 from discord.ext import commands
 
-bot = commands.Bot()
-
-
-youtube_dl.utils.bug_reports_message = lambda: ""
-
-ytdl_format_options = {
-    "format": "bestaudio/best",
-    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
-    "restrictfilenames": True,
-    "noplaylist": True,
-    "nocheckcertificate": True,
-    "ignoreerrors": False,
-    "logtostderr": False,
-    "quiet": True,
-    "no_warnings": True,
-    "default_search": "auto",
-    "source_address": "0.0.0.0",  
-}
-
-ffmpeg_options = {
-    'options': '-vn',
-    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-
-
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get("title")
-        self.url = data.get("url")
-        self.channel = data.get("channel")
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(
-            None, lambda: ytdl.extract_info(url, download=not stream)
-        )
-
-        if "entries" in data:
-            # take first item from a playlist
-            data = data["entries"][0]
-
-        filename = data["url"] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-
 class Music(commands.Cog):
-    def __init__(self, bot):
+    """Music cog to hold Wavelink related commands and listeners."""
+
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command()
-    async def join(self, ctx, *, channel: discord.VoiceChannel):
-        """Joins a voice channel"""
+        bot.loop.create_task(self.connect_nodes())
 
-        if ctx.voice_client is not None:
-            return await ctx.voice_client.move_to(channel)
+    async def connect_nodes(self):
+        """Connect to our Lavalink nodes."""
+        await self.bot.wait_until_ready()
 
-        await channel.connect()
+        await wavelink.NodePool.create_node(bot=self.bot,
+                                            host='127.0.0.1',
+                                            port=2333,
+                                            password='youshallnotpass')
 
-    @commands.command()
-    async def playfile(self, ctx, *, query):
-        """Plays a file from the local filesystem"""
-
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(
-            source, after=lambda e: print(f"Player error: {e}") if e else None
-        )
-
-        await ctx.send(f"Now playing: {query}")
-
-    @commands.command()
-    async def yt(self, ctx, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(
-                player, after=lambda e: print(f"Player error: {e}") if e else None
-            )
-
-        await ctx.send(f"Now playing: {player.title}")
-
-    
     @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        voice_state = member.guild.voice_client
-    
-            
-        if voice_state is None:
-                # Exiting if the bot it's not connected to a voice channel
-            return 
+    async def on_wavelink_node_ready(self, node: wavelink.Node):
+        """Event fired when a node has finished connecting."""
+        print(f'Node: <{node.identifier}> is ready!')
 
-        if len(voice_state.channel.members) == 1:
-            await voice_state.disconnect()
-   
     @commands.command()
-    async def play(self, ctx, *, url):
-        """Streams from a url (same as yt, but doesn't predownload)"""
+    async def play(self, ctx: commands.Context, *, search: wavelink.YouTubeTrack):
+        """Play a song with the given search query.
+        If not connected, connect to our voice channel.
+        """
+        if not ctx.voice_client:
+            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        else:
+            vc: wavelink.Player = ctx.voice_client
 
+        await vc.play(search)
 
-        channel = ctx.message.author.voice.channel
-        await ctx.guild.change_voice_state(channel=channel, self_mute=False, self_deaf=True)
+    @commands.command()
+    async def stop(self, ctx: commands.Context):
+        if not ctx.voice_client:
+            return await ctx.send("I am not currently in a voice channel!")
+        else:
+            vc: wavelink.Player = ctx.voice_client
+        await vc.stop()
+        await ctx.send("Stopped!")
 
-
-                
-        async with ctx.typing():
-            
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            ctx.voice_client.play(
-                
-                player, after=lambda e: print(f"Player error: {e}") if e else None,
-            )
-            
-        embed = discord.Embed(title=f"Now playing: **{player.title}**", description=f"**Uploaded by {player.channel}**")
-        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar)
+    @commands.command(aliases=['dc'])
+    async def disconnect(self, ctx:commands.Context):
+        if not ctx.voice_client:
+            return await ctx.send("I am not currently in a voice channel!")
         
-        await ctx.send(embed=embed)
+        await ctx.voice_client.disconnect(force=True)
 
-    
-    
-    @commands.command()
-    async def volume(self, ctx, volume: int):
-        """Changes the player's volume"""
+        await ctx.send("Disconected from current vc!")
+        
 
-        if ctx.voice_client is None:
-            return await ctx.send("Not connected to a voice channel.")
+async def setup(bot):
+    await bot.add_cog(Music(bot))
 
-        if volume > 100:
-            await ctx.send("You cannot put this over 100 Volume!")
-        else:
-            ctx.voice_client.source.volume = volume / 100
-            await ctx.send(f"Changed volume to {volume}%")
-
-    @commands.command()
-    async def stop(self, ctx):
-        """Stops and disconnects the bot from voice"""
-
-        if ctx.voice_client is None:
-            return await ctx.send("Not connected to a voice channel.")
-        else:
-            await ctx.voice_client.disconnect()
-            await ctx.send("Disconected!")
-
-    @commands.command()
-    async def lyrics(self, ctx, artist,*, title):
-        async with aiohttp.ClientSession() as session:
-           async with session.get(f"https://api.lyrics.ovh/v1/{artist}/{title}") as response:
-                data = await response.json()
-                lyrics = data['lyrics']
-                if lyrics is None:
-                    await ctx.send("Song not found! Please enter correct Artist and Song title")
-                if len(lyrics) > 2048:
-                    lyrics = lyrics[:2048]
-                emb = discord.Embed(title = f"{title}" , description = f"{lyrics}", color = 0xa3a3ff)
-                await ctx.send(embed=emb)
-        await session.close()
-    
-    @commands.command()
-    async def pause(self, ctx):
-        ctx.voice_client.pause()
-        await ctx.send(f"Paused!")
-
-    @commands.command()
-    async def resume(self, ctx):
-        ctx.voice_client.resume()
-        await ctx.send(f"Resumed!")
-    
-    @playfile.before_invoke
-    @yt.before_invoke
-    @play.before_invoke
-    async def ensure_voice(self, ctx):
-        if ctx.voice_client is None:
-            if ctx.author.voice:
-                await ctx.author.voice.channel.connect()
-            else:
-                await ctx.send("You are not connected to a voice channel.")
-                raise commands.CommandError("Author not connected to a voice channel.")
-        elif ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
-
-
-def setup(bot):
-    bot.add_cog(Music(bot))
 
